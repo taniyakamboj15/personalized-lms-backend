ed530ad189e49faa6c6344c2d23a90f4ebc84433
Taniya Kamboj <152772663+taniyakamboj15@users.noreply.github.com>
2025-07-21 22:36:47 +0530

    Update server.js

--- server.js
+++ server.js
@@ -42,7 +42,9 @@ const corsOptions = {
   },
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"],
 };
+
 app.use("/", limiter);
 app.use(cors(corsOptions));
 
